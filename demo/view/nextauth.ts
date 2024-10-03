a4bb56787120aa81506369fe17162796a30a1a6b
yingdi.liang <2039815501@qq.com>
2024-10-03 18:08:25 +0800

    fix:e登录前先注册

--- demo/view/nextauth.ts
+++ demo/view/nextauth.ts
@@ -1,6 +1,27 @@
 import NextAuth from "next-auth"
 import github from "next-auth/providers/github"
+import { action } from "./src/app/register/action";
+import { setUserId } from "./src/app/utils";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [github],
+    callbacks: {
+        async signIn({user}) {
+            if (user.name) {
+                const formData = new FormData();
+                formData.append('username', user.name);
+                formData.append('password', '');
+                try {
+                    const data = await action(formData);
+                    if(data.code === 0) {
+                        setUserId(data.data?.id || "");
+                    }
+                } catch (error) {
+                    alert(`catch error: ${(error as Error).message}`);
+                    throw new Error('123');
+                }
+            }
+            return true;
+        }
+    }
 })
--- demo/view/src/app/login/components/GithubPage.tsx
+++ demo/view/src/app/login/components/GithubPage.tsx
@@ -1,27 +1,17 @@
 'use client';
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import styles from "../../page.module.css";
 import Link from "next/link";
-import { action } from "../../register/action";
 import { useState } from "react";
 
 export const GithubPage = () => {
-    const { data: githubData, status } = useSession();
-    const [data, setData] = useState<resultType>();
+    const [error, setError] = useState<string>();
 
     const handleClick = async () => {
-        signIn('github', { callbackUrl: 'http://localhost:8080/register' });
-
-        if (githubData?.user?.name) {
-            const formData = new FormData();
-            formData.append('username', githubData.user.name);
-            formData.append('password', '');
-            try {
-                const data = await action(formData);
-                setData(data);
-            } catch (error) {
-                alert(`catch error: ${(error as Error).message}`);
-            }
+        try{
+            await signIn('github', { callbackUrl: 'http://localhost:8080/register' });
+        } catch(e) {
+            setError("Github登录失败，请联系管理")
         }
     }
 
@@ -29,7 +19,7 @@ export const GithubPage = () => {
         <div>
             <div className={styles.registerText}>AUTH_GITHUB_ID:{process.env.NEXT_PUBLIC_AUTH_GITHUB_ID}</div>
             <div className={styles.registerText}>NEXT_PUBLIC_AUTH_GITHUB_SECRET:{process.env.NEXT_PUBLIC_AUTH_GITHUB_SECRET}</div>
-            <div className={styles.error}>{data?.msg}</div>
+            <div className={styles.error}>{error}</div>
             <div className={styles.registerGitHubCenter}>
                 <img
                     src="https://authjs.dev/img/providers/github.svg"
